7826cae1e377cc4138fbf0e571ca04c4b039cf8b
Matej <dev@matej.mk>
2025-10-09 22:37:49 +0200

    Unit 6 - Build your first Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://matej-astro-blog-tutorial.netlify.app"
+  site: "https://matej-astro-blog-tutorial.netlify.app",
+  integrations: [preact()]
 });
